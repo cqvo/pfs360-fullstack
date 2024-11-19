61993e8b4df7430fbd5232f8d7d18556b83df537
Chris V <50410061+cqvo@users.noreply.github.com>
2024-11-19 08:39:18 -0500

    Sync

---
+++ src/routes/clients/[id]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	let { data }: { data: PageData } = $props();
+	const client = data.client[0];
+
+</script>
+
+<h1 class="h1">{client.companyName}</h1>
+<p>Link status:</p>
+<button class="btn">Create Link</button>
