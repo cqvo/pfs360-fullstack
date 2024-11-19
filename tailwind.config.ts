b28e6805b9d3d64b6c7ef00f260e8efdd33aedff
Chris V <50410061+cqvo@users.noreply.github.com>
2024-11-18 21:39:46 -0500

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import type { Config } from 'tailwindcss';
+import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
+import * as themes from '@skeletonlabs/skeleton/themes';
+
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [skeleton({ themes: [themes.mint] }), typography, forms]
+} satisfies Config;
