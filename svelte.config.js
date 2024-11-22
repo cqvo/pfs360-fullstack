b1bc27a9d0c5ed1d43cbadce587e8b7fd46a3141
Chris V <50410061+cqvo@users.noreply.github.com>
2024-11-22 00:47:36 -0500

    actually read the documentation this time

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ export default {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			runtime: 'node20',
+			runtime: 'nodejs20.x',
 		})
 	}
 };
