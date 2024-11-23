35d7a93b7df7cf28d558ba2886455509d619dc0f
Chris V <50410061+cqvo@users.noreply.github.com>
2024-11-23 15:55:18 -0500

    added logging to see where DATABASE_URL is not loading

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'drizzle-kit';
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.DATABASE_URL) throw new Error('drizzle.config.ts: DATABASE_URL is not set');
 
 export default defineConfig({
   out: './drizzle',
--- src/lib/server/database/index.ts
+++ src/lib/server/database/index.ts
@@ -3,7 +3,7 @@ import { neon } from '@neondatabase/serverless';
 import * as schema from './schema';
 import { DATABASE_URL } from '$lib/config';
 
-if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!DATABASE_URL) throw new Error('lib/database/index.ts: DATABASE_URL is not set');
 const client = neon(DATABASE_URL);
 const db = drizzle(client, { schema });
 
