59b19ad2c888f9963a805a8aecdeec889f747ad9
Chris V <50410061+cqvo@users.noreply.github.com>
2025-02-25 11:25:27 -0500

    fixed handle probably

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,12 +5,13 @@ import { handle as authHandle } from '$lib/auth';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const response = await authHandle({ event, resolve });
-	console.log('hooks.server.ts: authHandle response', response);
+	if (response) {
+		return response;
+	}
 
 	const session = await event.locals.auth();
 	if (session?.user?.email) {
 		const user = await User.findOne(session.user.email);
-		console.log('hooks.server.ts: user', user);
 		if (!user) {
 			throw new Error('hooks.server.ts: No user found.');
 		}
