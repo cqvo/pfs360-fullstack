8c45730787dd1a504e1db6eb2a2618e87d878cba
Chris V <50410061+cqvo@users.noreply.github.com>
2025-02-25 21:52:42 -0500

    deleted all the old pattern stuff

--- drizzle/0004_many_triton.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "fact_link_requests" ALTER COLUMN "request_status_id" SET DEFAULT 'Pending';--> statement-breakpoint
-ALTER TABLE "fact_link_requests" ALTER COLUMN "request_status_id" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "dim_items" ADD COLUMN "key_date" varchar;--> statement-breakpoint
-ALTER TABLE "dim_items" ADD COLUMN "key_iv" varchar;
--- src/lib/constants.ts
+++
@@ -1,7 +0,0 @@
-export enum Status {
-	Pending = 'Pending',
-	Expired = 'Expired',
-	Active = 'Active',
-	Completed = 'Completed',
-	Error = 'Error'
-}
