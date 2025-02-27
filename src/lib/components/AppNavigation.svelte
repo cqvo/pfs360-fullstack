64fef0c30b688b9c82ed622aa0afcdd45d08d53d
Chris Vo <50410061+cqvo@users.noreply.github.com>
2025-02-27 14:33:19 -0500

    Upgraded SvelteKit to 2.8.3, minor nav issues. (#24)
    Fixed minor bug in the navigation that was causing the page to not render when being run in development mode.
    Moved navigation into its own Svelte component.
    Changed navigation tiles to be generated from an array instead of manually defining them.
    Added a hover effect to the navigation tiles.

---
+++ src/lib/components/AppNavigation.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import '../../app.css';
+	import { Navigation } from '@skeletonlabs/skeleton-svelte';
+	import { signOut } from '@auth/sveltekit/client';
+
+	import LayoutGrid from 'lucide-svelte/icons/layout-grid';
+	import Users from 'lucide-svelte/icons/users';
+	import CloudUpload from 'lucide-svelte/icons/cloud-upload';
+	import Settings from 'lucide-svelte/icons/settings';
+	import Logs from 'lucide-svelte/icons/logs';
+	import LogOut from 'lucide-svelte/icons/log-out';
+
+	const navItems = [
+		{ tileProps: { label: 'Dashboard', href: '/app' }, icon: LayoutGrid },
+		{ tileProps: { label: 'Clients', href: '/app/clients' }, icon: Users },
+		{ tileProps: { label: 'Upload', href: '/app/upload' }, icon: CloudUpload },
+		{ tileProps: { label: 'Settings', href: '/app/settings' }, icon: Settings },
+		{ tileProps: { label: 'Changelog', href: '/app/changelog' }, icon: Logs },
+		{ tileProps: { label: 'Sign Out', onclick: signOut }, icon: LogOut }
+	];
+</script>
+
+{#each navItems as item, i}
+	<Navigation.Tile id={i.toString()} hover="hover:preset-tonal" width="w-24" {...item.tileProps}>
+		<item.icon />
+	</Navigation.Tile>
+{/each}
--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,58 +1,13 @@
 <script lang="ts">
 	import '../../app.css';
-	let { data, children } = $props();
-	import { AppBar, Navigation, ToastProvider } from '@skeletonlabs/skeleton-svelte';
-	import {
-		LayoutGrid,
-		Menu,
-		Users,
-		FileText,
-		CloudUpload,
-		Settings,
-		LogIn,
-		LogOut, Logs
-	} from 'lucide-svelte';
-	import { signIn, signOut } from '@auth/sveltekit/client';
+	import { ToastProvider } from '@skeletonlabs/skeleton-svelte';
+	import AppNavigation from '$lib/components/AppNavigation.svelte';
+
+	let { children } = $props();
 </script>
 
 <header class="flex h-min justify-center gap-2 p-2">
-	<Navigation.Tile label="Dashboard" href="/app" width="w-24">
-		<LayoutGrid />
-	</Navigation.Tile>
-	<Navigation.Tile label="Clients" href="/app/clients" width="w-24">
-		<Users />
-	</Navigation.Tile>
-	<Navigation.Tile label="Upload" href="/app/upload" width="w-24">
-		<CloudUpload />
-	</Navigation.Tile>
-	<Navigation.Tile label="Settings" href="/app/settings" width="w-24">
-		<Settings />
-	</Navigation.Tile>
-	<Navigation.Tile label="Changelog" href="/app/changelog" width="w-24">
-		<Logs />
-	</Navigation.Tile>
-	{#if !data.session}
-		<Navigation.Tile
-			id="signInTile"
-			label="Sign In"
-			width="w-24"
-			onclick={() => {
-				signIn();
-			}}
-		>
-			<LogIn />
-		</Navigation.Tile>
-	{:else}
-		<Navigation.Tile
-			id="signOutTile"
-			label="Sign Out"
-			width="w-24"
-			onclick={() => {
-				signOut();
-			}}
-		>
-			<LogOut />
-		</Navigation.Tile>{/if}
+	<AppNavigation />
 </header>
 
 <ToastProvider placement="top-end">
