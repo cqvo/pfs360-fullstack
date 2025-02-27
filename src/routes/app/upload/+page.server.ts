7e5b016655421055d52f4097a068598d5146f2f4
Chris V <50410061+cqvo@users.noreply.github.com>
2025-02-27 11:44:22 -0500

    added logger to upload/+page.server.ts

--- src/routes/app/upload/+page.server.ts
+++ src/routes/app/upload/+page.server.ts
@@ -1,31 +1,28 @@
-import type { Actions } from '../../../../.svelte-kit/types/src/routes';
-import { fail } from '@sveltejs/kit';
+import { fail, type Actions } from '@sveltejs/kit';
 import TaxdomeRecord from '$lib/apps/client/class/TaxdomeRecord';
+import { createLogger } from '$lib/server/LoggerFactory';
+
+const logger = createLogger({ component: '/routes/app/upload/+page.server.ts' });
 
 export const actions = {
 	uploadCsv: async ({ request }) => {
 		try {
-			console.log('Received uploadCsv POST');
 			const formData = await request.formData();
 			const file = formData.get('fileToUpload') as File;
-			console.log('Received file:', file?.name);
-			console.log('formData', formData);
 			if (!file || !file.name || file.name === 'undefined') {
 				return fail(400, {
 					error: true,
 					message: 'You must provide a file to upload'
 				});
 			}
+			logger.info('uploadCsv() Received file:', file.name);
 			TaxdomeRecord.processCsv(file).catch((err) => {
-				console.error('Error processing CSV in background:', err);
+				throw err;
 			});
 			return { success: true};
-		} catch (error) {
-			console.error('Error processing file:', error);
-			return fail(500, {
-				error: true,
-				message: 'Error processing file'
-			});
+		} catch (err) {
+			logger.error('uploadCsv() Error:', err);
+			return fail(500, err);
 		}
 	}
 } satisfies Actions;
