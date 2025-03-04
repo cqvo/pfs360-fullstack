be58ae4e50eedc405824588b939cf75b47a95692
Chris Vo <50410061+cqvo@users.noreply.github.com>
2025-03-03 23:35:14 -0500

    Deleted test pages. Updated LoggerFactory. (#27)

--- src/routes/app/test/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import FancyInput from './FancyInput.svelte';
-	let { data } = $props();
-	let message = $state(JSON.stringify(data.users));
-</script>
-
-<FancyInput bind:value={message} />
-<p>{message}</p>
--- src/routes/app/test/+page.ts
+++
@@ -1,9 +0,0 @@
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = async ({ fetch }) => {
-	const response = await fetch('/api/v1/users');
-	const users = await response.json();
-	return {
-		users
-	};
-};
